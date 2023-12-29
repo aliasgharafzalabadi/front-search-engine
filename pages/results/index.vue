f0fb0e1d59153b1a18c9c1f14d5aec745b957df2
Ali <aliasgharafzalabadi@gmail.com>
2023-12-29 14:49:07 +0330

    update

--- pages/results/index.vue
+++ pages/results/index.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
+    <v-row justify="center">
+        <v-col cols="12" md="8">
+            <v-text-field @keyup.enter="getData()" v-model="query.query" class="mx-auto mt-8" label="Search"
+                hide-details="auto" outlined dense flat solo solo-inverted rounded color="error" variant="solo-inverted"
+                prepend-inner-icon="mdi-magnify" style="max-width: 400px; width: 100%;">
+            </v-text-field>
+        </v-col>
+    </v-row>
+    <div class="d-flex flex-column align-center mt-4">
         <template v-for="(dock, index) in docks" :key="index">
-            <v-card loading :title="dock.title" :text="dock.summary" variant="tonal"></v-card>
+            <v-card width="600" loading :title="dock.title" :text="dock.summary" variant="tonal" link></v-card>
         </template>
     </div>
 </template>
@@ -12,7 +20,7 @@ const query = ref({
     chance: ""
 })
 const docks = ref([])
-onMounted(async() => {
+onMounted(async () => {
 
     setTimeout(() => {
         query.value = route.query
@@ -40,3 +48,16 @@ const getData = async () => {
     docks.value = data.value
 }
 </script>
+<style scoped>
+.d-flex {
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-start;
+    flex-wrap: wrap;
+}
+
+.v-card {
+    margin-top: 20px;
+    margin-bottom: 5px;
+}
+</style>
