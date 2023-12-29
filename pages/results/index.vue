93c506778df12db74f43ee4ee6f9a3d81ecd5874
Ali <aliasgharafzalabadi@gmail.com>
2023-12-29 18:02:30 +0330

    update

--- pages/results/index.vue
+++ pages/results/index.vue
@@ -7,13 +7,16 @@
             </v-text-field>
         </v-col>
     </v-row>
+    <div>
+        About  {{ query.query.length }}  results in   {{ time}}  ms
+    </div>
     <div class="d-flex flex-column align-center mt-4">
         <div v-if="isQueryFixed">
             <div class="fix">Did you mean:</div>
             <div>{{ fixedQuery }}</div>
         </div>
         <template v-for="(dock, index) in docks" :key="index">
-            <v-card width="600" loading :title="dock.title" :text="dock.summary" variant="tonal" link></v-card>
+            <v-card width="600" loading :title="dock.title" :text="dock.summary" variant="tonal" link @click="openlink(dock.url)"></v-card>
         </template>
     </div>
 </template>
@@ -22,6 +25,7 @@ const route = useRoute();
 const router = useRouter();
 const fixedQuery = ref("")
 const isQueryFixed= ref(false)
+const time = ref()
 const query = ref({
     query: "",
     chance: ""
@@ -38,7 +42,8 @@ onMounted(async () => {
 
 const getData = async () => {
 
-    router.push({path:'/results', query: {query : query.value , chance: byChance}})
+    router.push({path:'/results', query: {query : query.query}})
+    query.query = []
 
     const { data } = await useFetch('/', {
         baseURL: "http://127.0.0.1:5000",
@@ -55,6 +60,14 @@ const getData = async () => {
     console.log(data)
 
     docks.value = data.value[0]
+    fixedQuery.value = data.value[1]
+    time.value = data.value[2]
+    isQueryFixed.value = data.value[3]
+}
+
+const openlink = (url)=>{
+    window.open(url)
+
 }
 </script>
 <style scoped>
